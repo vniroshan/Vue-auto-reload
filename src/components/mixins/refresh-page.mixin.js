0599b86fbc0164448a42ff3cde01f46f68b3262b
vniroshan <nnirosh447@gmail.com>
2022-05-14 22:47:54 +0530

    test 1

---
+++ src/components/mixins/refresh-page.mixin.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+
+export const refreshPageMixin = {
+  data() {
+    return {
+      currentHash: '{{POST_BUILD_ENTERS_HASH_HERE}}',
+     // token: localStorage.getItem('user-token'),
+      hashChanged: false,
+      newHash: ''
+    }
+  },
+  methods: {
+    initVersionCheck(url, frequency = 3000) {
+      setInterval(() => {
+        this.checkVersion(url);
+      }, frequency);
+    },
+    async checkVersion(url) {
+      try {
+        const fileResponse = await axios.get(`${url}?t=${new Date().getTime()}`);
+
+        this.newHash = fileResponse.data.hash;
+
+        this.hashChanged = this.hasHashChanged(this.currentHash, this.newHash);
+      } catch (error) {
+        this.loading = false;
+        if (!error.response) {
+          this.errorStatus = 'Error: Network Error'
+        } else {
+          this.errorStatus = error.response.data.message;
+        }
+      }
+    },
+    hasHashChanged(currentHash, newHash) {
+      if (!currentHash || currentHash === '{{POST_BUILD_ENTERS_HASH_HERE}}') {
+        return true;
+      }
+
+      return currentHash !== newHash;
+    },
+    reloadApp() {
+      this.currentHash = this.newHash;
+      window.location.reload();
+    }
+  }
+};
